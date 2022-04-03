779ebabdbeee3abe67812cd15a6c80c290e519d7
EddieFAF <eddie.faf@googlemail.com>
2022-04-03 14:55:32 +0200

    ncmpcpp changed

--- run_once_01_install_packages.sh
+++ run_once_01_install_packages.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 #Install tools
-yay -S zsh vim emacs lf vifm ranger ueberzug
-yay -S fzf ripgrep exa
+yay -Sy --needed --noconfirm zsh vim emacs lf vifm ranger ueberzug
+yay -Sy --needed --noconfirm fzf ripgrep exa
 
 #install Fonts
-yay -S nerd-fonts-hack nerd-fonts-fira-code nerd-fonts-source-code-pro nerd-fonts-jetbrains-mono
+yay -Sy --needed --noconfirm nerd-fonts-hack
+yay -Sy --needed --noconfirm nerd-fonts-fira-code
+yay -Sy --needed --noconfirm nerd-fonts-source-code-pro
+yay -Sy --needed --noconfirm nerd-fonts-jetbrains-mono
 
 #install Starship
 curl -sS https://starship.rs/install.sh | sh
