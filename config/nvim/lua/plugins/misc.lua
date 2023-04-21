c891cc68ba49b88da4f171077561fd09e79c2128
EddieFAF <eddie.faf@googlemail.com>
2023-04-21 14:29:42 +0200

    nvim changed

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -45,15 +45,4 @@ return {
       colorscheme = "onedark",
     },
   },
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-        pyright = {},
-      },
-    },
-  },
 }
