f02b27f8a5bafb2e5287bb537e20076158191c4c
EddieFAF <eddie.faf@googlemail.com>
2023-11-09 06:55:08 +0100

    nvim

--- config/nvim-kickmini/init.lua
+++ config/nvim-kickmini/init.lua
@@ -99,7 +99,7 @@ require("lazy").setup({
       current_line_blame = true,
       current_line_blame_opts = {
         virt_text = true,
-        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+        virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
         delay = 1000,
       },
       sign_priority = 6,
