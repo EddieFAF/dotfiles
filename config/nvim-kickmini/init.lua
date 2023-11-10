31c0fa6a77e205be12bdb07506353dcb7a44d7f2
EddieFAF <eddie.faf@googlemail.com>
2023-11-10 13:05:28 +0100

    color palettes added

--- config/nvim-kickmini/init.lua
+++ config/nvim-kickmini/init.lua
@@ -245,31 +245,34 @@ require("mini.basics").setup({
   silent = false,
 })
 
--- [[ Color Palette Tokyo Night ]] -------------------------------------------
-local use_cterm, palette
-palette = {
-  base00 = "#1A1B26",
-  base01 = "#16161E",
-  base02 = "#2F3549",
-  base03 = "#444B6A",
-  base04 = "#787C99",
-  base05 = "#A9B1D6",
-  base06 = "#CBCCD1",
-  base07 = "#D5D6DB",
-  base08 = "#C0CAF5",
-  base09 = "#A9B1D6",
-  base0A = "#0DB9D7",
-  base0B = "#9ECE6A",
-  base0C = "#B4F9F8",
-  base0D = "#2AC3DE",
-  base0E = "#BB9AF7",
-  base0F = "#F7768E",
-}
+-- [[ Color Palette ]] -------------------------------------------------------
+require("colors.base16-everforest")
 
-if palette then
-  require('mini.base16').setup({ palette = palette, use_cterm = use_cterm })
-  vim.g.colors_name = "base16-tokyo-night-dark"
-end
+
+-- local use_cterm, palette
+-- palette = {
+--   base00 = "#1A1B26",
+--   base01 = "#16161E",
+--   base02 = "#2F3549",
+--   base03 = "#444B6A",
+--   base04 = "#787C99",
+--   base05 = "#A9B1D6",
+--   base06 = "#CBCCD1",
+--   base07 = "#D5D6DB",
+--   base08 = "#C0CAF5",
+--   base09 = "#A9B1D6",
+--   base0A = "#0DB9D7",
+--   base0B = "#9ECE6A",
+--   base0C = "#B4F9F8",
+--   base0D = "#2AC3DE",
+--   base0E = "#BB9AF7",
+--   base0F = "#F7768E",
+-- }
+--
+-- if palette then
+--   require('mini.base16').setup({ palette = palette, use_cterm = use_cterm })
+--   vim.g.colors_name = "base16-tokyo-night-dark"
+-- end
 
 -- [[ 'gc' to toggle comment ]] ----------------------------------------------
 require("mini.comment").setup()
