f5df9947993149945df0d40c3b71fe329557e25d
EddieFAF <eddie.faf@googlemail.com>
2023-12-05 16:00:21 +0100

    colors

--- config/minivim/init.lua
+++ config/minivim/init.lua
@@ -414,30 +414,6 @@ else
   require("colors.base16-tokyo-night-storm")
 end
 
--- local use_cterm, palette
--- palette = {
---   base00 = "#1A1B26",
---   base01 = "#16161E",
---   base02 = "#2F3549",
---   base03 = "#444B6A",
---   base04 = "#787C99",
---   base05 = "#A9B1D6",
---   base06 = "#CBCCD1",
---   base07 = "#D5D6DB",
---   base08 = "#C0CAF5",
---   base09 = "#A9B1D6",
---   base0A = "#0DB9D7",
---   base0B = "#9ECE6A",
---   base0C = "#B4F9F8",
---   base0D = "#2AC3DE",
---   base0E = "#BB9AF7",
---   base0F = "#F7768E",
--- }
---
--- if palette then
---   require('mini.base16').setup({ palette = palette, use_cterm = use_cterm })
---   vim.g.colors_name = "base16-tokyo-night-dark"
--- end
 
 -- [[ Bracketed ]] -----------------------------------------------------------
 require("mini.bracketed").setup()
