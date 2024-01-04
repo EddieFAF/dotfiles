b99d834a4ec38d2e929b47e6169e6de6146e5356
EddieFAF <eddie.faf@googlemail.com>
2024-01-04 15:52:57 +0100

    no comment

--- config/minivim/init.lua
+++ config/minivim/init.lua
@@ -192,31 +192,31 @@ require("lazy").setup({
         --      vim.cmd 'colorscheme tokyonight'
       end,
     },
-  {
-    'navarasu/onedark.nvim',
-    enabled = true,
-    lazy = false,
-    priority = 1000, -- Ensure it loads first
-    config = function()
-      require('onedark').setup {
-        transparent = true,
-        term_colors = true,
-        ending_tildes = false,
-        code_style = {
-          comments = 'italic',
-          keywords = 'bold',
-          functions = 'bold',
-          strings = 'none',
-          variables = 'italic',
-        },
-        diagnostics = {
-          darker = true,
-          undercurl = true,
-          background = true,
-        },
-      }
-    end,
-  },
+    {
+      'navarasu/onedark.nvim',
+      enabled = true,
+      lazy = false,
+      priority = 1000, -- Ensure it loads first
+      config = function()
+        require('onedark').setup {
+          transparent = true,
+          term_colors = true,
+          ending_tildes = false,
+          code_style = {
+            comments = 'italic',
+            keywords = 'bold',
+            functions = 'bold',
+            strings = 'none',
+            variables = 'italic',
+          },
+          diagnostics = {
+            darker = true,
+            undercurl = true,
+            background = true,
+          },
+        }
+      end,
+    },
 
     {
       -- show gitstatus in statuscolumn and more
