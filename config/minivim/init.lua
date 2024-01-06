ba955eef8012d118693dde30f6e495b1e725fd90
EddieFAF <eddie.faf@googlemail.com>
2024-01-06 19:22:12 +0100

    changes

--- config/minivim/init.lua
+++ config/minivim/init.lua
@@ -520,8 +520,8 @@ require("mini.basics").setup({
 -- [[ Color Palette ]] -------------------------------------------------------
 --require("colors.base16-dracula")
 if vim.fn.hostname() == "blackhole" then
-  --vim.cmd [[colorscheme onedark]]
-  require("colors.base16-onedark")
+  vim.cmd [[colorscheme onedark]]
+--  require("colors.base16-onedark")
 elseif vim.fn.hostname() == "atomium" then
   require("colors.base16-everforest")
 else
