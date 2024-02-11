b58f3990eb3501a0d9389667ae8971c127a55bf4
EddieFAF <eddie.faf@googlemail.com>
2024-02-11 08:38:35 +0100

    heirline reorganized

---
+++ config/nvim-mini/lua/plugins/heirline.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "rebelot/heirline.nvim",
+    lazy = false,
+    dependencies = {
+      "linrongbin16/lsp-progress.nvim",
+    },
+    config = function()
+      require("heirline").setup({
+        winbar = require("config.heirline.winbar"),
+        statusline = require("config.heirline.statusline"),
+        statuscolumn = require("config.heirline.statuscolumn"),
+        opts = {
+          disable_winbar_cb = function(args)
+            local conditions = require("heirline.conditions")
+
+            return conditions.buffer_matches({
+              buftype = { "nofile", "prompt", "help", "quickfix", "terminal" },
+              filetype = { "alpha", "oil", "lspinfo", "toggleterm" },
+            }, args.buf)
+          end,
+        },
+      })
+    end,
+  },
+  {
+    "linrongbin16/lsp-progress.nvim",
+    lazy = true,
+    config = function()
+      require("lsp-progress").setup({})
+    end,
+  },
+
+}
