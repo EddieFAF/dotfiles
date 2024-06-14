6007d29c291b7e513aaa2b25eb7dea1677641102
EddieFAF <eddie.faf@googlemail.com>
2024-06-14 12:45:41 +0200

    oil.nvim changed

--- config/nvim-mini/lua/plugins/oil.lua
+++ config/nvim-mini/lua/plugins/oil.lua
@@ -14,15 +14,23 @@ return {
         ["<C-v>"] = "actions.select_vsplit",
         ["<C-s>"] = "actions.select_split",
         ["<Esc>"] = "actions.close",
+        ["q"] = "actions.close",
       },
       view_options = {
         show_hidden = true,
+        natural_order = true,
+        is_always_hidden = function(name, _)
+          return name == ".." or name == ".git"
+        end,
       },
+      default_file_explorer = true,
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
       -- Configuration for the floating window in oil.open_float
       float = {
         -- Padding around the floating window
         padding = 5,
-        max_width = 0,
+        max_width = 90,
         max_height = 0,
         border = "single",
         win_options = {
