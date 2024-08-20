74d977296430d3d1b1c42fb65c9fdcbfce277912
EddieFAF <eddie.faf@googlemail.com>
2024-08-20 12:58:50 +0200

    yazi:username/hostname added to header

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,37 +1,9 @@
--- -- show symbolic links in statusline
--- function Status:name()
---   local h = cx.active.current.hovered
---   if not h then
---     return ui.Span("")
---   end
---
---   --	return ui.Span(" " .. h.name)
---   local linked = ""
---   if h.link_to ~= nil then
---     linked = " -> " .. tostring(h.link_to)
---   end
---   return ui.Span(" " .. h.name .. linked)
--- end
-
--- -- Show Username/Hostname in Header
--- function Header:host()
---   if ya.target_family() ~= "unix" then
---     return ui.Line {}
---   end
---   return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
--- end
---
--- function Header:render(area)
---   self.area = area
---
---   local right = ui.Line { self:count(), self:tabs() }
---   --	local left = ui.Line { self:cwd(math.max(0, area.w - right:width())) }
---   local left = ui.Line { self:host(), self:cwd(math.max(0, area.w - right:width())) }
---   return {
---     ui.Paragraph(area, { left }),
---     ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
---   }
--- end
+Header:children_add(function()
+  if ya.target_family() ~= "unix" then
+    return ui.Line {}
+  end
+  return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
 
 Status:children_add(function()
   local h = cx.active.current.hovered
