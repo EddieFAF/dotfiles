ff9e737d0ea4e632d2635cdd02d9bb13d03aa4ea
EddieFAF <eddie.faf@googlemail.com>
2025-05-12 10:57:52 +0200

    yazi and nvim

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,23 +1,23 @@
 
-function Linemode:custom()
-    local year = os.date("%Y")
-    local time = (self._file.cha.modified or 0) // 1
-
-    if time > 0 and os.date("%Y", time) ~= year then
-        time = os.date("%b %d %H:%M", time)
-    else
-        time = time and os.date("%b %d  %Y", time) or ""
-    end
-
-    local size = self._file:size()
-    return ui.Line(string.format(" %s %s ", size and ya.readable_size(size):gsub(" ", "") or "-", time))
-end
+-- function Linemode:custom()
+--     local year = os.date("%Y")
+--     local time = (self._file.cha.modified or 0) // 1
+--
+--     if time > 0 and os.date("%Y", time) ~= year then
+--         time = os.date("%b %d %H:%M", time)
+--     else
+--         time = time and os.date("%b %d  %Y", time) or ""
+--     end
+--
+--     local size = self._file:size()
+--     return ui.Line(string.format(" %s %s ", size and ya.readable_size(size):gsub(" ", "") or "-", time))
+-- end
 
 function Linemode:file_info()
   return string.format("%s   %s", self:size(), self:mtime())
 end
 
-require("full-border"):setup()
+--require("full-border"):setup()
 
 require("yatline"):setup({
 --theme = my_theme,
@@ -108,6 +108,7 @@ style_a = {
       section_c = {
         {type = "string", custom = false, name = "hovered_file_extension", params = {true}},
         {type = "coloreds", custom = false, name = "permissions"},
+        {type = "string", custom = false, name = "hovered_ownership"},
       }
     }
   },
