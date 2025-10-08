15190267cf2c141f689fb99dbbe618cc71ac1753
EddieFAF <eddie.faf@googlemail.com>
2025-10-08 09:17:26 +0200

    yazi fixed

--- config/nvim-2025v3/lua/plugins/yazi.lua
+++ config/nvim-2025v3/lua/plugins/yazi.lua
@@ -1,27 +1,22 @@
 MiniDeps.later(function()
-    MiniDeps.add({
-        source = "mikavilpas/yazi.nvim",
-        depends = {
-            "nvim-lua/plenary.nvim",
-        },
-    })
+  MiniDeps.add {
+    source = 'mikavilpas/yazi.nvim',
+    depends = {
+      'nvim-lua/plenary.nvim',
+    },
+  }
 
-    require("yazi.nvim").setup({
+  require('yazi').setup {
 
-        ---@type YaziConfig | {}
-        opts = {
-            open_for_directories = true,
-            keymaps = {
-                show_help = "<f1>",
-            },
-        },
-    })
-    vim.keymap.set({ "n", "v" }, "<leader>.", "<cmd>Yazi<cr>", { desc = "Open yazi at the current file" })
-    vim.keymap.set(
-        "n",
-        "<leader>ew",
-        "<cmd>Yazi cwd<cr>",
-        { desc = "Open the file manager in nvim's working directory" }
-    )
-    vim.keymap.set("n", "<c-up>", "<cmd>Yazi toggle<cr>", { desc = "Resume the last yazi session" })
+    ---@type YaziConfig | {}
+    opts = {
+      open_for_directories = true,
+      keymaps = {
+        show_help = '<f1>',
+      },
+    },
+  }
+  vim.keymap.set({ 'n', 'v' }, '<leader>.', '<cmd>Yazi<cr>', { desc = 'Open yazi at the current file' })
+  vim.keymap.set('n', '<leader>ew', '<cmd>Yazi cwd<cr>', { desc = "Open the file manager in nvim's working directory" })
+  vim.keymap.set('n', '<c-up>', '<cmd>Yazi toggle<cr>', { desc = 'Resume the last yazi session' })
 end)
