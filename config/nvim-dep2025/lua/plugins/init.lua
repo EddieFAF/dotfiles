aac3a6ff6446a7bba82eaeffff68c74b86c349c9
EddieFAF <eddie.faf@googlemail.com>
2026-02-11 11:06:45 +0100

    removed several old nvim configs

--- config/nvim-dep2025/lua/plugins/init.lua
+++
@@ -1,7 +0,0 @@
--- Order is important! Please don't change this order
-require("plugins.appearance")
-require("plugins.mason")
-require("plugins.languages")
-require("plugins.editing")
-require("plugins.coding")
-require("plugins.bindings")
